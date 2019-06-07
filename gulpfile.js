ece8cf749c37c644b896200288f2c51e0b2fd43d
Adam <adam.lj.haglund@gmail.com>
2019-06-07 18:19:35 +0200

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,74 @@
-const Gulp = require('gulp')
+const { src, dest, watch, series, parallel } = require('gulp')
+const ifG = require('gulp-if')
 const Autoprefixer = require('gulp-autoprefixer')
-const Babel = require('gulp-babel')
 const CleanCSS = require('gulp-clean-css')
+const Sass = require('gulp-sass')
 const Pug = require('gulp-pug')
+const Babel = require('gulp-babel')
 const Uglify = require('gulp-uglify')
+const SourceMaps = require('gulp-sourcemaps')
+const Delete = require('delete')
+const Pages = require('gh-pages')
 
-Gulp.task('html', () =>
-  Gulp.src('src/**/*.pug')
-    .pipe(Pug())
-    .pipe(Gulp.dest('dist')),
-)
+const destination = 'dist'
 
-Gulp.task('css', () =>
-  Gulp.src('src/**/*.css')
-    .pipe(CleanCSS())
+const ignoreInitial = false
+const isProd = process.env.NODE_ENV === 'production'
+const ifProd = m => ifG(isProd, m)
+
+Sass.compiler = require('node-sass')
+
+// Pug -> HTML
+const htmlPath = 'src/*.pug'
+const html = () =>
+  src(htmlPath)
+    .pipe(Pug({ locals: { isProd } }))
+    .pipe(dest(destination))
+
+// SCSS -> CSS
+const cssPath = 'src/**/*.scss'
+const css = () =>
+  src(cssPath)
+    .pipe(SourceMaps.init())
+    .pipe(Sass().on('error', Sass.logError))
     .pipe(Autoprefixer())
-    .pipe(Gulp.dest('dist')),
-)
+    .pipe(ifProd(CleanCSS()))
+    .pipe(SourceMaps.write('.'))
+    .pipe(dest(destination))
 
-Gulp.task('js', () =>
-  Gulp.src('src/**/*.js')
-    .pipe(
-      Babel({
-        presets: [
-          [
-            '@babel/env',
-            {
-              targets: {
-                browsers: browserList,
-              },
-            },
-          ],
-        ],
-      }),
-    )
-    .pipe(
-      Uglify({
-        toplevel: true,
-      }),
-    )
-    .pipe(Gulp.dest('dist')),
-)
+// TS -> JS
+const jsPath = 'src/**/*.ts'
+const js = () => {
+  const tsResult = src(jsPath)
+    .pipe(SourceMaps.init())
+    .pipe(Babel())
 
-Gulp.task('rest', () =>
-  Gulp.src(['src/**/*.svg', 'src/**/*.ico']).pipe(Gulp.dest('dist')),
-)
+  return tsResult
+    .pipe(ifProd(Uglify({ toplevel: true })))
+    .pipe(SourceMaps.write('.'))
+    .pipe(dest(destination))
+}
 
-Gulp.task('default', ['html', 'css', 'js', 'rest'])
+// Copy files
+const restPaths = ['src/**/*.{png,jpg,svg,ico,mp4}', 'src/CNAME']
+const rest = () => src(restPaths).pipe(dest(destination))
 
-Gulp.task('watch', ['default'], () => Gulp.watch('src/**/*', ['default']))
+// Delete files
+const clean = () => Delete.promise('dist/*')
+
+exports.clean = clean
+
+exports.watch = cb => {
+  clean()
+  watch(htmlPath, { ignoreInitial }, html)
+  watch(cssPath, { ignoreInitial }, css)
+  watch(jsPath, { ignoreInitial }, js)
+  watch(restPaths, { ignoreInitial }, rest)
+  cb()
+}
+
+exports.publish = cb => {
+  Pages.publish('dist', cb)
+}
+
+exports.default = series(clean, parallel(html, css, js, rest))
