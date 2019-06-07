e555a74dcac0a0e749f020fd1dbb1176ecdb6309
Adam <adam.lj.haglund@gmail.com>
2019-06-07 23:55:49 +0200

    wip

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,56 +5,52 @@ interface ICompany {
 
 const MICROSOFT = {
   name: 'Microsoft',
-  logo: 'microsoft.svg'
+  logo: 'microsoft.svg',
 }
 
 const BETHESDA = {
   name: 'Bethesda',
-  logo: 'bethesda.svg'
+  logo: 'bethesda.svg',
 }
 
 const DEVOLVER_DIGITAL = {
   name: 'Devolver Digital',
-  logo: 'devolver_digital.svg'
+  logo: 'devolver_digital.svg',
 }
 
 const UPLOADVR = {
   name: 'UploadVR',
-  logo: 'upload_vr.svg'
+  logo: 'upload_vr.svg',
 }
 
 const PC_GAMING = {
-  name: 'PcGaming',
-  logo: 'pc_gaming.svg'
+  name: 'PC Gaming Show',
+  logo: 'pc_gaming.svg',
 }
 
 const LIMITED_RUN = {
   name: 'Limited Run',
-  logo: 'limited_run.svg'
+  logo: 'limited_run.svg',
 }
 
 const UBISOFT = {
   name: 'Ubisoft',
-  logo: 'ubisoft.svg'
+  logo: 'ubisoft.svg',
 }
 
 const KINDA_FUNNY_SHOWCASE = {
   name: 'Kinda Funny Showcase',
-  logo: 'kinda_funny_showcase.svg'
+  logo: 'kinda_funny_showcase.svg',
 }
 
 const SQUARE_ENIX = {
   name: 'Square Enix',
-  logo: 'square_enix.svg'
+  logo: 'square_enix.svg',
 }
 
 const NINTENDO_DIRECT = {
   name: 'Nintendo Direct',
-  logo: 'nintendo_direct.svg'
-}
-
-enum YEAR {
-  NINETEEN = '2019',
+  logo: 'nintendo_direct.svg',
 }
 
 interface ISchedule {
@@ -68,79 +64,102 @@ const getDate = (
   year: number,
   hour: number,
   minute: number,
-) => new Date(`${day} ${month} ${year} ${hour}:${minute} UTC`)
+) => new Date(`${month} ${day} ${year} ${hour}:${minute} UTC`)
 
 // @ts-ignore
-const SCHEDULES: { [key in YEAR]: ISchedule[] } = {
-  [YEAR.NINETEEN]: [
-    {
-      company: MICROSOFT,
-      time: getDate(9, 6, 2019, 20, 0),
-    },
-    {
-      company: BETHESDA,
-      time: getDate(10, 6, 2019, 0, 30),
-    },
-    {
-      company: DEVOLVER_DIGITAL,
-      time: getDate(10, 6, 2019, 2, 0),
-    },
-    {
-      company: UPLOADVR,
-      time: getDate(10, 6, 2019, 16, 0),
-    },
-    {
-      company: PC_GAMING,
-      time: getDate(10, 6, 2019, 17, 0),
-    },
-    {
-      company: LIMITED_RUN,
-      time: getDate(10, 6, 2019, 19, 0),
-    },
-    {
-      company: UBISOFT,
-      time: getDate(10, 6, 2019, 20, 0),
-    },
-    {
-      company: KINDA_FUNNY_SHOWCASE,
-      time: getDate(10, 6, 2019, 23, 30),
-    },
-    {
-      company: SQUARE_ENIX,
-      time: getDate(11, 6, 2019, 1, 0),
-    },
-    {
-      company: NINTENDO_DIRECT,
-      time: getDate(11, 6, 2019, 16, 0),
-    },
-  ],
-}
-
+const SCHEDULE: ISchedule[] = [
+  {
+    company: MICROSOFT,
+    time: getDate(9, 6, 2019, 20, 0),
+  },
+  {
+    company: BETHESDA,
+    time: getDate(10, 6, 2019, 0, 30),
+  },
+  {
+    company: DEVOLVER_DIGITAL,
+    time: getDate(10, 6, 2019, 2, 0),
+  },
+  {
+    company: UPLOADVR,
+    time: getDate(10, 6, 2019, 16, 0),
+  },
+  {
+    company: PC_GAMING,
+    time: getDate(10, 6, 2019, 17, 0),
+  },
+  {
+    company: LIMITED_RUN,
+    time: getDate(10, 6, 2019, 19, 0),
+  },
+  {
+    company: UBISOFT,
+    time: getDate(10, 6, 2019, 20, 0),
+  },
+  {
+    company: KINDA_FUNNY_SHOWCASE,
+    time: getDate(10, 6, 2019, 23, 30),
+  },
+  {
+    company: SQUARE_ENIX,
+    time: getDate(11, 6, 2019, 1, 0),
+  },
+  {
+    company: NINTENDO_DIRECT,
+    time: getDate(11, 6, 2019, 16, 0),
+  },
+]
 
 class Conference {
-  private el: HTMLDivElement
+  private schedule: ISchedule
+  private el: string
 
   constructor(schedule: ISchedule) {
-    this.el = document.createElement('div')
-    this.el.classList.add('conference')
-    this.el.innerHTML = schedule.company.name
+    this.schedule = schedule
+    this.el = `
+      <div class="conference">
+        <div class="company">${schedule.company.name}</div>
+        <div class="time">${this.getTime()}</div>
+      </div>
+    `
+  }
+
+  private getTime() {
+    return Intl.DateTimeFormat(undefined, {
+      hour: 'numeric',
+      minute: 'numeric',
+      timeZoneName: 'short',
+    }).format(this.schedule.time)
+  }
+
+  public getDayOfMonth() {
+    return this.schedule.time.getDate()
   }
 
   public appendTo(element: HTMLElement) {
-    return element.append(this.el)
+    element.insertAdjacentHTML('beforeend', this.el)
   }
 }
 
 const updatePage = () => {
   const container = document.getElementById('container')!
 
-  const schedule = SCHEDULES['2019']
+  const conferences = SCHEDULE.map(c => new Conference(c))
 
-  schedule.forEach(item => {
-    console.log(item.company.name, item.time)
-    const conference = new Conference(item)
+  let dayContainer: HTMLDivElement = null as any
+  let lastDay = -1
 
-    conference.appendTo(container)
+  conferences.forEach(c => {
+    const dayOfMonth = c.getDayOfMonth()
+
+    if (lastDay !== dayOfMonth) {
+      lastDay = dayOfMonth
+
+      dayContainer = document.createElement('div')
+      container.append(dayContainer)
+    }
+
+    c.appendTo(dayContainer)
   })
 }
 
